a7db1ede0e071d1e58391bf2fa9570cb10587a4b
James Tan <jamest01230@gmail.com>
2026-08-14 15:17:56 -0400

    add

---
+++ A_Chewbaсca_and_Number.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ------------------------------------------------------------ */
+/* ------------------------------------------------------------ */
+template <typename T, typename = void>
+struct is_container : std::false_type
+{
+};
+template <typename T>
+struct is_container<
+    T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>>
+    : std::true_type
+{
+};
+template <typename T>
+typename std::enable_if<!is_container<T>::value && !std::is_same<T, std::string>::value>::type
+print(const T &arg)
+{
+    std::cout << arg;
+}
+void print(const std::string &arg)
+{
+    std::cout << '"' << arg << '"';
+}
+template <typename T1, typename T2>
+void print(const std::pair<T1, T2> &p)
+{
+    std::cout << "(";
+    print(p.first);
+    std::cout << ", ";
+    print(p.second);
+    std::cout << ")";
+}
+template <typename Container>
+typename std::enable_if<is_container<Container>::value>::type
+print(const Container &c)
+{
+    std::cout << "[";
+    auto it = c.begin();
+    while (it != c.end())
+    {
+        print(*it);
+        if (++it != c.end())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]";
+}
+template <typename T, typename... Args>
+void print(const T &first, const Args &...rest)
+{
+    print(first);
+    if (sizeof...(rest) > 0)
+    {
+        std::cout << " ";
+        print(rest...);
+    }
+}
+template <typename... Args>
+void println(const Args &...args)
+{
+    print(args...);
+    std::cout << std::endl;
+}
+#define PRINT(var)                  \
+    do                              \
+    {                               \
+        std::cout << #var << " = "; \
+        print(var);                 \
+        std::cout << std::endl;     \
+    } while (0)
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int INT_INF = numeric_limits<int>::max();
+const long long LL_INF = numeric_limits<long long>::max();
+
+#define int long long
+/* ------------------------------------------------------------ */
+/* ------------------------------------------------------------ */
+
+int32_t main()
+{
+    fastio;
+
+    string x;
+    cin >> x;
+
+    string newNumber;
+
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (i == 0)
+        {
+            if (x[i] - '0' >= 5 && x[i] - '0' < 9)
+            {
+                newNumber.push_back('0' + (9 - (x[i] - '0')));
+            }
+            else
+            {
+                newNumber.push_back(x[i]);
+            }
+        }
+        else
+        {
+            if (x[i] - '0' >= 5)
+            {
+                newNumber.push_back('0' + (9 - (x[i] - '0')));
+            }
+            else
+            {
+                newNumber.push_back(x[i]);
+            }
+        }
+    }
+
+    cout << newNumber << endl;
+
+    return 0;
+}
